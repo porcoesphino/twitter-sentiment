afd18d72ecd03ba829fb2d4e83cee2519ad6177f
Bodey Baker <bodey.baker@gmail.com>
2013-10-23 19:52:03 +0900

    added integration with twitter4j using a stream

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
-twitter-sentiment
-=================
+#twitter-sentiment#
 
 A small project to get the current "sentiment" on some companies based on recent tweets.
+
+## Authentication ##
+
+Authentication is required for use of the 1.1 Twitter API.
+This application uses application-only auth.
+Please go to the [Twitter Developers][] site to get the following:
+
+ - Consumer key
+ - Consumer secret
+ - Access token
+ - Access token secret
+
+Add them to the `twitter.properties` file.
+
+[Twitter Developers]: https://dev.twitter.com/apps
--- src/main/java/com/porcoesphino/twitterSentiment/App.java
+++ src/main/java/com/porcoesphino/twitterSentiment/App.java
@@ -1,11 +1,58 @@
 package com.porcoesphino.twitterSentiment;
 
+import twitter4j.StallWarning;
+import twitter4j.Status;
+import twitter4j.StatusDeletionNotice;
+import twitter4j.StatusListener;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+
+
 /**
- * Hello world!
+ * The main class to query the twitter server and display popular words in
+ * real time.
+ * 
+ * @author bodey.baker@gmail.com
  *
  */
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
-    }
+	public static void main(String[] args) {
+		
+		TwitterStream twitterStream = new TwitterStreamFactory()
+				.getInstance();
+
+		StatusListener listener = new StatusListener() {
+			public void onStatus(Status status) {
+				System.out.println("@" + status.getUser().getScreenName()
+						+ " - " + status.getText());
+			}
+
+			public void onDeletionNotice(
+					StatusDeletionNotice statusDeletionNotice) {
+				System.out.println("Got a status deletion notice id:"
+						+ statusDeletionNotice.getStatusId());
+			}
+
+			public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+				System.out.println("Got track limitation notice:"
+						+ numberOfLimitedStatuses);
+			}
+
+			public void onScrubGeo(long userId, long upToStatusId) {
+				System.out.println("Got scrub_geo event userId:" + userId
+						+ " upToStatusId:" + upToStatusId);
+			}
+
+			public void onStallWarning(StallWarning warning) {
+				System.out.println("Got stall warning:" + warning);
+			}
+
+			public void onException(Exception ex) {
+				ex.printStackTrace();
+			}
+		};
+		
+		twitterStream.addListener(listener);
+		twitterStream.sample();
+	}
 }
