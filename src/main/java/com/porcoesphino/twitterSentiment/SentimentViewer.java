bc05235a9b0c87904cfbb0c980a7715f1a0dbf50
Bodey Baker <bodey.baker@gmail.com>
2013-12-23 01:18:51 +0900

    The GUI needs cleanup but is visually complete.

--- src/main/java/com/porcoesphino/twitterSentiment/SentimentViewer.java
+++ src/main/java/com/porcoesphino/twitterSentiment/SentimentViewer.java
@@ -1,27 +1,27 @@
 package com.porcoesphino.twitterSentiment;
 
+import java.awt.CardLayout;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.Timer;
+import java.util.TimerTask;
 
-import javax.swing.JFrame;
+import javax.swing.AbstractListModel;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JTable;
 import javax.swing.SpinnerNumberModel;
-
-import java.io.IOException;
-import java.text.ParseException;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.TreeSet;
-
-import javax.swing.JPanel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import javax.swing.table.AbstractTableModel;
@@ -31,7 +31,18 @@ import net.miginfocom.swing.MigLayout;
 
 public class SentimentViewer {
 
+	private static final long serialVersionUID = 1L;
 	public final LinkedList<String> viewingTickers = new LinkedList<String>();
+	
+	class Tweet {
+		public final String user;
+		public final String message;
+		
+		public Tweet(String user, String message) {
+			this.user = user;
+			this.message = message;
+		}
+	}
 	
 	public static final int addSorted(String string, LinkedList<String> list) {
 		for (int listIndex = 0; listIndex < list.size(); listIndex++) {
@@ -56,9 +67,57 @@ public class SentimentViewer {
 		return -1;
 	}
 	
-	@SuppressWarnings("serial")
+	class TableCompaniesViewing extends AbstractTableModel {
+		
+		private static final long serialVersionUID = SentimentViewer.serialVersionUID;
+
+		public Object getValueAt(int rowIndex, int columnIndex) {
+			if (rowIndex == viewingTickers.size()) {
+				switch (columnIndex) {
+					case 0:
+						return "";
+					case 1:
+						return "<html><div style=\"color:red;\">Unmatched Tweets</div></html>";
+					default:
+						return "0";
+				}
+			}
+			String ticker = viewingTickers.get(rowIndex);
+			switch (columnIndex) {
+				case 0:
+					return ticker;
+				case 1:
+					return SandP500Lookup.getCompanyName(ticker);
+				default:
+					return "0";
+			}
+		}
+		
+		public int getRowCount() {
+			return viewingTickers.size() + 1;
+		}
+		
+		public int getColumnCount() {
+			return 3;
+		}
+		
+		@Override
+		public String getColumnName(int column) {
+			switch (column) {
+			case 0:
+				return "Ticker";
+			case 1:
+				return "Name";
+			default:
+				return "Count";
+			}
+		}
+	}
+	
 	class TableCompaniesToView extends AbstractTableModel {
 		
+		private static final long serialVersionUID = SentimentViewer.serialVersionUID;
+
 		public void addCompany(String ticker) {
 			if (viewingTickers.contains(ticker)) {
 				return;
@@ -98,6 +157,8 @@ public class SentimentViewer {
 		}
 	}
 	
+	private TableCompaniesViewing tableModelViewing =
+	    new TableCompaniesViewing();
 	private JFrame frame;
 
 	/**
@@ -134,27 +195,24 @@ public class SentimentViewer {
 		initialize();
 	}
 
-	@SuppressWarnings("serial")
-	private void setStartingPanel(final JPanel panelContent, final JButton btnStart, final JSpinner spinnerInterval) {
-		
-		panelContent.removeAll();
+	private JPanel generateStartingPanel() {
 		
 		final TableCompaniesToView companiesToView = new TableCompaniesToView();
 		
-		JPanel startContent = new JPanel();
-		
-		startContent.setLayout(new MigLayout("fill"));
-		panelContent.add(startContent, new CC().grow().push());
+		JPanel panelStart = new JPanel();
+		panelStart.setLayout(new MigLayout("fill"));
 		
 		JLabel lblAll = new JLabel("S&P Companies (Select to watch)");
-		startContent.add(lblAll, new CC());
+		panelStart.add(lblAll, new CC());
 		
 		JLabel lblSelected = new JLabel("Companies To Watch (Select to remove)");
-		startContent.add(lblSelected, new CC().wrap());
+		panelStart.add(lblSelected, new CC().wrap());
 		
 		final JTable tableAll = new JTable();
+		
 		tableAll.setModel(new AbstractTableModel() {
 			
+			private static final long serialVersionUID = SentimentViewer.serialVersionUID;
 			String[] tickers = SandP500Lookup.getTickers();
 			Boolean sorted = false;
 			
@@ -196,7 +254,7 @@ public class SentimentViewer {
 			}
 		});
 		JScrollPane scrollPaneAll = new JScrollPane(tableAll);
-		startContent.add(scrollPaneAll, new CC().grow().pushX().growX());
+		panelStart.add(scrollPaneAll, new CC().grow().pushX().growX());
 		
 		final JTable tableSelected = new JTable();
 		tableSelected.setModel(companiesToView);
@@ -212,74 +270,90 @@ public class SentimentViewer {
 			}
 		});
 		JScrollPane scrollPaneSelected = new JScrollPane(tableSelected);
-		startContent.add(scrollPaneSelected, new CC().grow().pushX().growX());
+		panelStart.add(scrollPaneSelected, new CC().grow().pushX().growX());
+		
+		return panelStart;
 	}
 	
-	@SuppressWarnings("serial")
-	private void setRunningPanel(final JPanel panelContent, final JButton btnStart, final JSpinner spinnerInterval) {
+	private JPanel generateRunningPanel() {
 		
-		panelContent.removeAll();
-		
-		JPanel runningContent = new JPanel();
-		
-		runningContent.setLayout(new MigLayout("fill"));
-		panelContent.add(runningContent, new CC().grow().push());
+		JPanel panelRunning = new JPanel();
+		panelRunning.setLayout(new MigLayout("fill"));
 		
 		JLabel lblSelected = new JLabel("Companies");
-		runningContent.add(lblSelected, new CC().wrap());
+		panelRunning.add(lblSelected, new CC());
+		
+		final JLabel lblFocus = new JLabel("");
+		panelRunning.add(lblFocus, new CC().wrap());
 		
 		final JTable tableAll = new JTable();
-		tableAll.setModel(new AbstractTableModel() {
+		tableAll.setModel(tableModelViewing);
+		JScrollPane scrollPaneAll = new JScrollPane(tableAll);
+		panelRunning.add(scrollPaneAll, new CC().grow().pushX().growX());
+		
+		final LinkedList<Tweet> displayedTweets = new LinkedList<Tweet>();
+		for (int i = 0; i < 20; i++) {
+			displayedTweets.add(new Tweet("porcoesphino",
+			    "I really, really hope this works!!! 123455kdfjkasldfjas"
+			    + "kldfakjflksadjfasldkfjaslkdfjalsdfjsald)"));
+		}
+		
+		final JList<String> listTweets = new JList<String>(new AbstractListModel<String>() {
+
+			private static final long serialVersionUID = SentimentViewer.serialVersionUID;
 			
-			public Object getValueAt(int rowIndex, int columnIndex) {
-				String ticker = viewingTickers.get(rowIndex);
-				switch (columnIndex) {
-					case 0:
-						return ticker;
-					case 1:
-						return SandP500Lookup.getCompanyName(ticker);
-					default:
-						return "0";
-				}
+			public int getSize() {
+				return displayedTweets.size();
 			}
-			
-			public int getRowCount() {
-				return viewingTickers.size();
-			}
-			
-			public int getColumnCount() {
-				return 3;
-			}
-			
-			@Override
-			public String getColumnName(int column) {
-				switch (column) {
-				case 0:
-					return "Ticker";
-				case 1:
-					return "Name";
-				default:
-					return "Count";
-				}
+
+			public String getElementAt(int index) {
+				Tweet tweet = displayedTweets.get(index);
+				String response = "<html><div style=\"padding:2px;"
+				    + "background-color:#EDF5F4;color:black\">"
+				    + "<div style=\"padding:2px;font-weight:500;\">"
+				    + "@" + tweet.user
+				    + "</div><p style=\"text-wrap:break-word;\">"
+				    + tweet.message
+				    + "</p></div></html>";
+				return response;
 			}
 		});
+		
+		JScrollPane scrollPaneTweets = new JScrollPane(listTweets);
+		panelRunning.add(scrollPaneTweets, new CC().grow().pushX().growX().wrap());
+		
+		JLabel lblPreMissed = new JLabel("Unreceived tweets due to track limitation: " +
+		    0 + " @ " + new Date());
+		panelRunning.add(lblPreMissed, new CC().span(2));
+		
 		tableAll.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
 			
 			public void valueChanged(ListSelectionEvent e) {
 				for (int rowIndex : tableAll.getSelectedRows() ) {
 					String ticker = (String) tableAll.getValueAt(rowIndex, 0);
 					String name = (String) tableAll.getValueAt(rowIndex, 1);
+					String displayName;
+					if (name.contains("<html>")) {
+						displayName = "Unknown Tweets";
+					} else {
+						displayName = name + " (" + ticker + ")";
+					}
+					lblFocus.setText((new Date()).toString() + ": " + displayName);
 				}
 			}
 		});
-		JScrollPane scrollPaneAll = new JScrollPane(tableAll);
-		runningContent.add(scrollPaneAll, new CC().grow().pushX().growX());
+		
+		return panelRunning;
 	}
 	
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		
+		final String msgStart = "Start";
+		final String msgRunning = "Stop";
+		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 800, 600);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -287,22 +361,25 @@ public class SentimentViewer {
 		    new MigLayout("fill"));
 		
 		final JPanel panelContent = new JPanel();
-		panelContent.setLayout(new MigLayout("fill"));
+		final CardLayout cardLayout = new CardLayout();
+		panelContent.setLayout(cardLayout);
+		final JPanel panelStarting = generateStartingPanel();
+		final JPanel panelRunning = generateRunningPanel();
+		panelContent.add(panelStarting, msgStart);
+		panelContent.add(panelRunning, msgRunning);
 		frame.getContentPane().add(panelContent, new CC().dockNorth().grow().push().wrap());
 		
-		JPanel panelCommon = new JPanel();
-		//TODO: CardLayout?
-		panelContent.setLayout(new MigLayout("fill"));
+		final JPanel panelCommon = new JPanel();
+		panelCommon.setLayout(new MigLayout("fill"));
 		frame.getContentPane().add(panelCommon, new CC().dockSouth().growX().pushX());
 		
-		JLabel labelInterval = new JLabel("Interval (mins)");
+		final String strInterval = "Interval (mins)";
+		final JLabel labelInterval = new JLabel(strInterval);
 		panelCommon.add(labelInterval);
 		
 		final JSpinner spinnerInterval = new JSpinner(new SpinnerNumberModel(5, .5, 60, 1));
 		panelCommon.add(spinnerInterval, new CC());
 		
-		final String msgStart = "Start";
-		final String msgRunning = "Stop";
 		final JButton btnStart = new JButton(msgStart);
 		panelCommon.add(btnStart, new CC().grow());
 		
@@ -312,23 +389,44 @@ public class SentimentViewer {
 				if (btnStart.getText().contentEquals(msgStart)) {
 					btnStart.setText(msgRunning);
 					spinnerInterval.setEnabled(false);
-					setRunningPanel(panelContent, btnStart, spinnerInterval);
+					labelInterval.setToolTipText("Until there are tweets from "
+					    + "the full interval, this indicator will be red");
+					labelInterval.setText("<html><font style=\"color:red\">"
+					    + strInterval
+					    + "</font></html>");
+					long waitInMilliSeconds =
+					    ((Double) spinnerInterval.getValue()).longValue()
+					    * 60 * 1000;
+					Timer timer = new Timer();
+					timer.schedule(new TimerTask() {
+						@Override
+						public void run() {
+							labelInterval.setText(
+							    "<html><font style=\"color:green\">"
+							    + strInterval
+							    + "Interval (mins)</font></html>");
+						}
+					}, waitInMilliSeconds);
+					cardLayout.last(panelContent);
+					tableModelViewing.fireTableDataChanged();
 				} else if (btnStart.getText().contentEquals(msgRunning)) {
 					btnStart.setText(msgStart);
 					spinnerInterval.setEnabled(true);
-					setStartingPanel(panelContent, btnStart, spinnerInterval);
+					labelInterval.setToolTipText("");
+					labelInterval.setText("Interval (mins)");
+					cardLayout.first(panelContent);
 				} else {
 					throw new RuntimeException("Button out of Sync. No idea how.");
 				}
 			}
 		});
 		
+		cardLayout.first(panelContent);
+		
 		addSorted("GOOG", viewingTickers);
 		addSorted("AMZN", viewingTickers);
 		addSorted("AAPL", viewingTickers);
 		addSorted("MSFT", viewingTickers);
-		
-		setStartingPanel(panelContent, btnStart, spinnerInterval);
 	}
 
 }
